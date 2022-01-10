f7e4be5291637caf41d9358a848e249a72b24db0
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-10 21:21:00 +0100

    Initial commit

---
+++ lua/no-clown-fiesta/dark/Whichkey.lua
@@ -0,0 +1,9 @@
+local Whichkey = {
+  WhichKey = { fg = C.light_gray },
+  WhichKeySeperator = { fg = C.gray },
+  WhichKeyGroup = { fg = C.yellow },
+  WhichKeyDesc = { fg = C.light_gray },
+  WhichKeyFloat = { bg = C.bg },
+}
+
+return Whichkey
