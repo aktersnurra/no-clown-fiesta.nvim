de0ccae36e7ed2b572a21c3de6ba74dfbdc291ff
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 20:44:37 +0100

    Fix typo dark

--- lua/no-clown-fiesta/init.lua
+++ lua/no-clown-fiesta/init.lua
@@ -2,19 +2,19 @@ vim.api.nvim_command "hi clear"
 if vim.fn.exists "syntax_on" then
   vim.api.nvim_command "syntax reset"
 end
-vim.o.background = "dark"
+vim.o.background = "no-clown-fiesta"
 vim.o.termguicolors = true
-vim.g.colors_name = "dark"
+vim.g.colors_name = "no-clown-fiesta"
 
-local util = require "dark.util"
-Config = require "dark.config"
-C = require "dark.palette"
-local highlights = require "dark.highlights"
-local Treesitter = require "dark.Treesitter"
-local markdown = require "dark.markdown"
-local Whichkey = require "dark.Whichkey"
-local Git = require "dark.Git"
-local LSP = require "dark.LSP"
+local util = require "no-clown-fiesta.util"
+Config = require "no-clown-fiesta.config"
+C = require "no-clown-fiesta.palette"
+local highlights = require "no-clown-fiesta.highlights"
+local Treesitter = require "no-clown-fiesta.Treesitter"
+local markdown = require "no-clown-fiesta.markdown"
+local Whichkey = require "no-clown-fiesta.Whichkey"
+local Git = require "no-clown-fiesta.Git"
+local LSP = require "no-clown-fiesta.LSP"
 
 local skeletons = {
   highlights,
