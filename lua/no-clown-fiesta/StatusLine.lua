fc492c30aef72ec034b472ef6780d4e7cca068ff
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-18 22:10:17 +0100

    Revert "Update StatusLine to bg for inactive"
    This reverts commit 0138b34718d78d3a0d0211b6ddd5f933441632d4.

--- lua/no-clown-fiesta/StatusLine.lua
+++ lua/no-clown-fiesta/StatusLine.lua
@@ -1,6 +1,6 @@
 local StatusLine = {
 	StatusLine = { fg = C.bg, bg = C.gray },
-	StatusLineNC = { fg = C.bg, bg = C.gray },
+	StatusLineNC = { fg = C.alt_bg, bg = C.gray },
 	StatusLineSeparator = { fg = C.alt_bg },
 	StatusLineTerm = { fg = C.alt_bg },
 	StatusLineTermNC = { fg = C.alt_bg },
