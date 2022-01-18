5de7f7d46a6c39663c1ba3043f14079bad59eebb
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-18 20:43:41 +0100

    Update nvim-tree colors 9

--- lua/no-clown-fiesta/NvimTree.lua
+++ lua/no-clown-fiesta/NvimTree.lua
@@ -1,8 +1,8 @@
 local NvimTree = {
 	NvimTreeFolderIcon = { fg = C.blue },
-	NvimTreeIndentMarker = { fg = C.accent },
-	NvimTreeNormal = { fg = C.light_gray, bg = C.accent },
-	NvimTreeVertSplit = { fg = C.accent, bg = C.accent },
+	NvimTreeIndentMarker = { fg = C.alt_bg },
+	NvimTreeNormal = { fg = C.light_gray, bg = C.alt_bg },
+	NvimTreeVertSplit = { fg = C.alt_bg, bg = C.alt_bg },
 	NvimTreeFolderName = { fg = C.blue },
 	NvimTreeOpenedFolderName = { fg = C.blue, style = "bold,italic" },
 	NvimTreeEmptyFolderName = { fg = C.gray, style = "italic" },
