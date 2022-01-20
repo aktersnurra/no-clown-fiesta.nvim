822e15fe660dd93a0cb6f45e8ea4d0b30687e529
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-20 23:47:13 +0100

    Update Selection and Border color in Telescope

--- lua/no-clown-fiesta/Telescope.lua
+++ lua/no-clown-fiesta/Telescope.lua
@@ -1,8 +1,8 @@
 local Telescope = {
 	TelescopeNormal = { fg = C.fg, bg = C.bg },
-	TelescopeSelection = { fg = C.light_gray, bg = C.accent },
+	TelescopeSelection = { fg = C.fg, bg = C.accent },
 	TelescopeMatching = { fg = C.info_yellow, style = "bold" },
-	TelescopeBorder = { fg = C.light_gray, bg = C.bg },
+	TelescopeBorder = { fg = C.medium_gray, bg = C.bg },
 }
 
 return Telescope
