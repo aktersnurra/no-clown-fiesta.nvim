7567355c95c0c770dde9ff1c3775495adeecb5de
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-09 01:46:58 +0200

    Add more hl groups to neogit

--- lua/no-clown-fiesta/Neogit.lua
+++ lua/no-clown-fiesta/Neogit.lua
@@ -1,5 +1,12 @@
 local Neogit = {
+  NeogitNotificationInfo = { fg = C.green },
+  NeogitNotificationWarning = { fg = C.warning_orange },
+  NeogitNotificationError = { fg = C.red },
   NeogitDiffAddHighlight = { fg = C.green, bg = C.alt_bg },
+  NeogitDiffDeleteHighlight = { fg = C.red, bg = C.alt_bg },
+  NeogitDiffContextHighlight = { fg = C.fg, bg = C.alt_bg },
+  NeogitHunkHeader = { fg = C.fg, bg = C.alt_bg },
+  NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.alt_bg },
 }
 
 return Neogit
