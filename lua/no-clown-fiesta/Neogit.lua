d7bc04cb2fbfc56c40815e975cb1447cc316cb72
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-10 01:29:00 +0200

    Update neogit colors

--- lua/no-clown-fiesta/Neogit.lua
+++ lua/no-clown-fiesta/Neogit.lua
@@ -2,11 +2,11 @@ local Neogit = {
   NeogitNotificationInfo = { fg = C.green },
   NeogitNotificationWarning = { fg = C.warning_orange },
   NeogitNotificationError = { fg = C.red },
-  NeogitDiffAddHighlight = { fg = C.green, bg = C.gray },
-  NeogitDiffDeleteHighlight = { fg = C.red, bg = C.gray },
-  NeogitDiffContextHighlight = { fg = C.fg, bg = C.gray },
-  NeogitHunkHeader = { fg = C.fg, bg = C.accent },
-  NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.accent },
+  NeogitDiffAddHighlight = { fg = C.green, bg = C.accent },
+  NeogitDiffDeleteHighlight = { fg = C.red, bg = C.accent },
+  NeogitDiffContextHighlight = { fg = C.fg, bg = C.accent },
+  NeogitHunkHeader = { fg = C.fg, bg = C.gray_blue },
+  NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.gray },
 }
 
 return Neogit
