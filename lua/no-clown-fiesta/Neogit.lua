36f710fd63cbb8856076e2340e7146eb54b978b0
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-10 01:34:54 +0200

    Update neogit with sign colors

--- lua/no-clown-fiesta/Neogit.lua
+++ lua/no-clown-fiesta/Neogit.lua
@@ -2,11 +2,11 @@ local Neogit = {
   NeogitNotificationInfo = { fg = C.green },
   NeogitNotificationWarning = { fg = C.warning_orange },
   NeogitNotificationError = { fg = C.red },
-  NeogitDiffAddHighlight = { fg = C.green, bg = C.accent },
-  NeogitDiffDeleteHighlight = { fg = C.red, bg = C.accent },
+  NeogitDiffAddHighlight = { fg = C.sign_add, bg = C.accent },
+  NeogitDiffDeleteHighlight = { fg = C.sign_delete, bg = C.accent },
   NeogitDiffContextHighlight = { fg = C.fg, bg = C.accent },
-  NeogitHunkHeader = { fg = C.fg, bg = C.gray_blue },
-  NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.gray },
+  NeogitHunkHeader = { fg = C.fg, bg = C.gray },
+  NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.gray_blue },
 }
 
 return Neogit
