eea2ecc003065ac46561af7a6f593f21bf7e2c80
Marco Galli <marco.galli@protonmail.com>
2022-07-10 00:50:07 +0200

    Run stylua

--- colors/no-clown-fiesta.lua
+++ colors/no-clown-fiesta.lua
@@ -1,5 +1,7 @@
 for k in pairs(package.loaded) do
-  if k:match(".*no-clown-fiesta.*") then package.loaded[k] = nil end
+  if k:match ".*no-clown-fiesta.*" then
+    package.loaded[k] = nil
+  end
 end
 
-require('no-clown-fiesta')
+require "no-clown-fiesta"
