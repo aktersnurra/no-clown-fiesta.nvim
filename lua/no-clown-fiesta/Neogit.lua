f26af0f287d20e6db0f3fe3451369a6a56e03761
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-10 02:08:39 +0200

    Change bg neogit

--- lua/no-clown-fiesta/Neogit.lua
+++ lua/no-clown-fiesta/Neogit.lua
@@ -2,11 +2,11 @@ local Neogit = {
   NeogitNotificationInfo = { fg = C.sign_add },
   NeogitNotificationWarning = { fg = C.warning_orange },
   NeogitNotificationError = { fg = C.sign_delete },
-  NeogitDiffAddHighlight = { fg = C.sign_add, bg = C.accent },
-  NeogitDiffDeleteHighlight = { fg = C.sign_delete, bg = C.accent },
-  NeogitDiffContextHighlight = { fg = C.fg, bg = C.accent },
-  NeogitHunkHeader = { fg = C.fg, bg = C.gray },
-  NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.gray_blue },
+  NeogitDiffAddHighlight = { fg = C.sign_add, bg = C.alt_bg },
+  NeogitDiffDeleteHighlight = { fg = C.sign_delete, bg = C.alt_bg },
+  NeogitDiffContextHighlight = { fg = C.fg, bg = C.alt_bg },
+  NeogitHunkHeader = { fg = C.fg, bg = C.alt_bg },
+  NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.accent },
 }
 
 return Neogit
