2c7de87a54020b05df328ce0bb804d47ebd33fa7
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-11 00:09:06 +0200

    Add color to neogit add/del text

--- lua/no-clown-fiesta/Neogit.lua
+++ lua/no-clown-fiesta/Neogit.lua
@@ -2,13 +2,13 @@ local Neogit = {
   NeogitNotificationInfo = { fg = C.sign_add },
   NeogitNotificationWarning = { fg = C.warning_orange },
   NeogitNotificationError = { fg = C.sign_delete },
-  NeogitDiffAddHighlight = { fg = C.fg, bg = C.neogit_light_green },
-  NeogitDiffDeleteHighlight = { fg = C.fg, bg = C.neogit_light_red },
+  NeogitDiffAddHighlight = { fg = C.sign_add, bg = C.neogit_light_green },
+  NeogitDiffDeleteHighlight = { fg = C.sign_delete, bg = C.neogit_light_red },
   NeogitDiffContextHighlight = { fg = C.light_gray, bg = C.alt_bg },
   NeogitHunkHeader = { fg = C.fg, bg = C.alt_bg },
   NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.accent },
-  NeogitDiffAdd = { fg = C.fg, bg = C.neogit_green },
-  NeogitDiffDelete = { fg = C.fg, bg = C.neogit_red },
+  NeogitDiffAdd = { fg = C.sign_add, bg = C.neogit_green },
+  NeogitDiffDelete = { fg = C.sign_delete, bg = C.neogit_red },
 }
 
 return Neogit
