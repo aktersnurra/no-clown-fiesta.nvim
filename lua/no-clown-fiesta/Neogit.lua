c932346f37285b81cef6c07b3a3422a286ec937c
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-11 22:38:28 +0200

    Update colors for neogit again

--- lua/no-clown-fiesta/Neogit.lua
+++ lua/no-clown-fiesta/Neogit.lua
@@ -2,13 +2,13 @@ local Neogit = {
   NeogitNotificationInfo = { fg = C.sign_add },
   NeogitNotificationWarning = { fg = C.warning_orange },
   NeogitNotificationError = { fg = C.sign_delete },
-  NeogitDiffAddHighlight = { fg = C.sign_add, bg = C.neogit_green },
-  NeogitDiffDeleteHighlight = { fg = C.sign_delete, bg = C.neogit_red },
-  NeogitDiffContextHighlight = { fg = C.fg, bg = C.bg },
+  NeogitDiffAddHighlight = { fg = C.sign_add, bg = C.neogit_light_green },
+  NeogitDiffDeleteHighlight = { fg = C.sign_delete, bg = C.neogit_light_red },
+  NeogitDiffContextHighlight = { fg = C.fg, bg = C.accent },
   NeogitHunkHeader = { fg = C.fg, bg = C.alt_bg },
   NeogitHunkHeaderHighlight = { fg = C.fg, bg = C.accent },
-  NeogitDiffAdd = { fg = C.sign_add, bg = C.neogit_light_green },
-  NeogitDiffDelete = { fg = C.sign_delete, bg = C.neogit_light_red },
+  NeogitDiffAdd = { fg = C.sign_add, bg = C.neogit_green },
+  NeogitDiffDelete = { fg = C.sign_delete, bg = C.neogit_red },
 }
 
 return Neogit
