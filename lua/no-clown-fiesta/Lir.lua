e7cc5923866a79c32804f8c36fa00bf285a0d250
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-14 01:02:30 +0200

    Update Lir colors

--- lua/no-clown-fiesta/Lir.lua
+++ lua/no-clown-fiesta/Lir.lua
@@ -1,8 +1,8 @@
 local Lir = {
-  LirFloatNormal = { fg = C.light_gray, bg = C.alt_bg },
-  LirDir = { fg = C.blue },
+  LirFloatNormal = { fg = C.fg, bg = C.alt_bg },
+  LirDir = { fg = C.white },
   LirSymLink = { fg = C.cyan },
-  LirEmptyDirText = { fg = C.blue },
+  LirEmptyDirText = { fg = C.medium_gray },
 }
 
 return Lir
