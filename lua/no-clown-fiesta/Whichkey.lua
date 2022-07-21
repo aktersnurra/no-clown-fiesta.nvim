5d071b6d04bd28920bd8f851b82d62a862d01151
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-21 23:04:44 +0200

    Set which group to red

--- lua/no-clown-fiesta/Whichkey.lua
+++ lua/no-clown-fiesta/Whichkey.lua
@@ -1,7 +1,7 @@
 local Whichkey = {
   WhichKey = { fg = C.light_gray },
   WhichKeySeperator = { fg = C.gray },
-  WhichKeyGroup = { fg = C.yellow },
+  WhichKeyGroup = { fg = C.red },
   WhichKeyDesc = { fg = C.light_gray },
   WhichKeyFloat = { bg = C.bg },
 }
