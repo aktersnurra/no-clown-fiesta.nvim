98ce7ca44e2f4f1a0b532650c098f9398d0049ad
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-29 18:22:41 +0200

    Update cmp colors

--- lua/no-clown-fiesta/nvim-cmp.lua
+++ lua/no-clown-fiesta/nvim-cmp.lua
@@ -3,9 +3,9 @@ local M = {}
 function M.highlight(palette)
   return {
     CmpItemAbbr = { fg = palette.fg },
-    CmpItemAbbrDeprecated = { fg = palette.orange },
-    CmpItemAbbrMatch = { fg = palette.info_yellow },
-    CmpItemAbbrMatchFuzzy = { fg = palette.info_yellow },
+    CmpItemAbbrDeprecated = { fg = palette.red },
+    CmpItemAbbrMatch = { fg = palette.orange },
+    CmpItemAbbrMatchFuzzy = { fg = palette.orange },
     CmpItemMenu = { fg = palette.medium_gray },
     CmpItemKind = { fg = palette.light_gray },
   }
