346db4e9795beb76b702d16d33e102478efefd18
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-10-13 00:34:06 +0200

    Remove undercurl support

--- lua/no-clown-fiesta/highlights.lua
+++ lua/no-clown-fiesta/highlights.lua
@@ -7,7 +7,7 @@ function M.highlight(palette, opts)
     MsgArea = { fg = palette.fg, bg = palette.bg },
     ModeMsg = { fg = palette.fg, bg = palette.bg },
     MsgSeparator = { fg = palette.fg, bg = palette.bg },
-    SpellBad = { sp = palette.error_red, undercurl = true },
+    SpellBad = { sp = palette.error_red, style = "underline" },
     SpellCap = { fg = palette.yellow, style = "underline" },
     SpellLocal = { fg = palette.green, style = "underline" },
     SpellRare = { fg = palette.purple, style = "underline" },
