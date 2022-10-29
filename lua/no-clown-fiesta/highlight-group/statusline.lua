2a811fa58f67333e2abead85ef76d969f7916070
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-10-29 20:49:00 +0200

    Update StatusLineNC

--- lua/no-clown-fiesta/highlight-group/statusline.lua
+++ lua/no-clown-fiesta/highlight-group/statusline.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.highlight(palette)
   return {
     StatusLine = { fg = palette.gray, bg = palette.bg },
-    StatusLineNC = { fg = palette.gray, bg = palette.bg },
+    StatusLineNC = { fg = palette.alt_bg, bg = palette.alt_bg },
     StatusLineSeparator = { fg = palette.bg },
     StatusLineTerm = { fg = palette.bg },
     StatusLineTermNC = { fg = palette.bg },
