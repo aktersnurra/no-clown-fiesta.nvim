9dcbcbb85c0dccd10b41453cc01cb8f3a49735aa
Denis Maciel <denismaciel@users.noreply.github.com>
2023-04-16 13:22:42 +0200

    Add support to leap.nvim (#19)

--- lua/no-clown-fiesta/groups/init.lua
+++ lua/no-clown-fiesta/groups/init.lua
@@ -4,6 +4,7 @@ return {
   require "no-clown-fiesta.groups.highlights",
   require "no-clown-fiesta.groups.hop",
   require "no-clown-fiesta.groups.lazy",
+  require "no-clown-fiesta.groups.leap",
   require "no-clown-fiesta.groups.lir",
   require "no-clown-fiesta.groups.lsp",
   require "no-clown-fiesta.groups.markdown",
