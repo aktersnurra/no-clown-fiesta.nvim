d6578a9156b92803bb1621ad6912f5c60d4f142c
Gustaf Rydholm <gustaf@nexure.io>
2024-07-09 13:09:40 +0200

    Update label to red

--- lua/no-clown-fiesta/groups/leap.lua
+++ lua/no-clown-fiesta/groups/leap.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.highlight(palette, opts)
   return {
     LeapMatch = { fg = palette.cyan, bold = true },
-    LeapLabel = { fg = palette.orange, bold = true },
+    LeapLabel = { fg = palette.red, bold = true },
     LeapBackdrop = { fg = palette.medium_gray },
   }
 end
