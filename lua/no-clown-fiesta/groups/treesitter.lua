2fc78155472320a01db180b0a3ea1e04e4abebca
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2024-07-22 00:56:56 +0200

    Update diff.delta again

--- lua/no-clown-fiesta/groups/treesitter.lua
+++ lua/no-clown-fiesta/groups/treesitter.lua
@@ -16,7 +16,11 @@ function M.highlight(palette, opts)
     ["@const.macro"] = { fg = palette.cyan },
     ["@constant"] = { fg = palette.white },
     ["@constructor"] = { fg = palette.cyan },
-    ["@diff.delta"] = { fg = palette.fg, bg = palette.sign_change },
+    ["@diff.delta"] = {
+      fg = palette.sign_add,
+      bg = palette.accent_green,
+      bold = true,
+    },
     ["@error"] = { fg = palette.error },
     ["@function"] = vim.tbl_extend(
       "force",
